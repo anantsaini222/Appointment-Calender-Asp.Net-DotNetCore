eb1881911fcbd980737c7dd0a947bcc9d4821592
Saini <AR8I38C@apac.vwg>
2021-10-02 19:11:49 +0530

    first commit

---
+++ Models/Event.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCalendar.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        //Relational data
+        public virtual Location Location { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public Event(IFormCollection form, Location location, ApplicationUser user)
+        {
+            User = user;
+            Name = form["Event.Name"].ToString();
+            Description = form["Event.Description"].ToString();
+            StartTime = DateTime.Parse(form["Event.StartTime"].ToString());
+            EndTime = DateTime.Parse(form["Event.EndTime"].ToString());
+            Location = location;
+        }
+
+        public void UpdateEvent(IFormCollection form, Location location, ApplicationUser user)
+        {
+            User = user;
+            Name = form["Event.Name"].ToString();
+            Description = form["Event.Description"].ToString();
+            StartTime = DateTime.Parse(form["Event.StartTime"].ToString());
+            EndTime = DateTime.Parse(form["Event.EndTime"].ToString());
+            Location = location;
+        }
+
+        public Event()
+        {
+
+        }
+    }
+}
---
+++ Models/ViewModels/EventViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCalendar.Models.ViewModels
+{
+    public class EventViewModel
+    {
+        public Event Event { get; set; }
+        public List<SelectListItem> Location = new List<SelectListItem>();
+        public string LocationName { get; set; }
+        public string UserId { get; set; }
+
+        public EventViewModel(Event myevent, List<Location> locations, string userid)
+        {
+            Event = myevent;
+            LocationName = myevent.Location.Name;
+            UserId = userid;
+            foreach (var loc in locations)
+            {
+                Location.Add(new SelectListItem() { Text = loc.Name });
+            }
+        }
+
+        public EventViewModel(List<Location> locations, string userid)
+        {
+            UserId = userid;
+            foreach (var loc in locations)
+            {
+                Location.Add(new SelectListItem() { Text = loc.Name });
+            }
+        }
+
+        public EventViewModel()
+        {
+
+        }
+
+    }
+}
